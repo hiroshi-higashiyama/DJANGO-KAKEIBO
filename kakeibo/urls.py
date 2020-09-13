5a685dbd5f9f4dfdfa3f2eea76e2f1ab4510f957
hiroshi <s20840011@gmail.com>
2020-09-13 17:49:13 +0900

    second

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import KakeiboListView
+from .views import KakeiboListView, KakeiboCreateView, create_done, KakeiboUpdateView, update_done, KakeiboDeleteView, delete_done
 
 app_name = 'kakeibo'
 
 urlpatterns = [
     path('kakeibo_list/', KakeiboListView.as_view(), name='kakeibo_list'),
+    path('kakeibo_create/', KakeiboCreateView.as_view(), name='kakeibo_create'),
+    path('create_done/', create_done, name='create_done'),
+    path('update/<int:pk>', KakeiboUpdateView.as_view(), name='kakeibo_update'),
+    path('update_done/', update_done, name='update_done'),
+    path('delete/<int:pk>/', KakeiboDeleteView.as_view(), name='kakeibo_delete'),
+    path('delete_done/', delete_done, name='delete_done'),
 ]
--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, UpdateView, DetailView
+from .forms import KakeiboForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Category, Kakeibo
 
 
@@ -10,3 +12,34 @@ class KakeiboListView(ListView):
 
     def queryset(self):
         return Kakeibo.objects.all()
+
+
+class KakeiboCreateView(CreateView):
+    model = Kakeibo
+    form_class = KakeiboForm
+    template_name = 'kakeibo/kakeibo_form.html'
+    success_url = reverse_lazy('kakeibo:create_done')
+
+
+def create_done(request):
+    return render(request, 'kakeibo/create_done.html')
+
+
+class KakeiboUpdateView(UpdateView):
+    model = Kakeibo
+    form_class = KakeiboForm
+    template_name = 'kakeibo/kakeibo_form.html'
+    success_url = reverse_lazy('kakeibo:update_done')
+
+
+def update_done(request):
+    return render(request, 'kakeibo/update_done.html')
+
+
+class KakeiboDeleteView(DeleteView):
+    model = Kakeibo
+    success_url = reverse_lazy('kakeibo:delete_done')
+
+
+def delete_done(request):
+    return render(request, 'kakeibo/delete_done.html')
